78d9e23f1c06ce9c5caa9f28ea75c2f8b309949f
Rashedul <rashedul.gen@gmail.com>
2025-03-24 11:03:08 -0700

    blastp analysis

---
+++ benchmark/blastp/blastp_analysis.sh
@@ -0,0 +1,48 @@
+#!bin/bash
+
+cd ../data/
+
+mkdir temp
+
+# blastp db
+for f in uniprot_sprot_[1-8]*.fasta; do 
+    echo "Processing: $f"; 
+    makeblastdb -in "$f" -dbtype prot -out temp/"${f%.fasta}_blastp_db";
+done
+
+# predict
+for db in temp/uniprot_sprot_5000_blastp_db temp/uniprot_sprot_10000_blastp_db temp/uniprot_sprot_20000_blastp_db temp/uniprot_sprot_40000_blastp_db temp/uniprot_sprot_80000_blastp_db; do 
+    echo "DB name: $db"
+
+    # Extract database name without path for cleaner output filenames
+    db_name=$(basename "$db")
+
+    blastp -query "uniprot_sprot_5000.fasta" -db "$db" \
+           -out "uniprot_sprot_5000_vs_${db_name}_blastp_results.txt" \
+           -num_threads 4 \
+           -outfmt "6 qseqid sseqid stitle evalue" \
+           -max_target_seqs 1
+done
+
+# remove duplicated lines
+for f in *_blastp_db_blastp_results.txt; do 
+    echo $f; 
+    awk '!seen[$1]++ && count[$1]++ == 0' $f >../benchmark/blastp/$f.duprem.txt;
+done
+
+# create id file of reference
+less uniprot_sprot_5000.fasta | grep ">" | awk '{ gsub(">", ""); print $1 "\t" $0}' | awk '{ match($0, /GN=([^ ]+)/, arr); if (arr[1]) print arr[1] "\t" $0}' >../benchmark/blastp/uniprot_sprot_5000.fasta.id
+
+rm *_blastp_db_blastp_results.txt
+rm -r temp
+
+# awk '{ match($0, /GN=([^ ]+)/, arr); if (arr[1]) print arr[1] "\t" $0}' uniprot_sprot_5000_vs_uniprot_sprot_10000_blastp_db_blastp_results.txt.duprem.txt | head
+# k=uniprot_sprot_5000_vs_uniprot_sprot_10000_blastp_db_blastp_results.txt.duprem.txt
+
+cd ../benchmark/blastp/
+
+# count correct prediction
+for k in *_db_blastp_results.txt.duprem.txt; do
+echo $k;
+comm -12 <(less uniprot_sprot_5000.fasta.id | awk '{print $2"|"$1}' | sort) <( less $k | awk '{ match($0, /GN=([^ ]+)/, arr); if (arr[1]) print arr[1] "\t" $0}' | awk '{print $2"|"$1}' | sort) | wc -l;
+done 
--- benchmark/pipelines.sh
+++ benchmark/pipelines.sh
@@ -1,42 +1,19 @@
-cd /mnt/c/PA-SigPro-Pipeline/data/
+#!bin/bash
 
-# blastp db
-for f in uniprot_sprot_[1-8]*.fasta; do 
-    echo "Processing: $f"; 
-    makeblastdb -in "$f" -dbtype prot -out temp/"${f%.fasta}_blastp_db";
+cd ../data/
+
+mkdir temp
+
+for f in uniprot_sprot*fasta; do
+    python3 db_build.py \
+        --fasta_path "$f" \
+        --db "${f}_db.pkl"
 done
 
-# predict
-for db in temp/uniprot_sprot_5000_blastp_db temp/uniprot_sprot_10000_blastp_db temp/uniprot_sprot_20000_blastp_db temp/uniprot_sprot_40000_blastp_db temp/uniprot_sprot_80000_blastp_db; do 
-    echo "DB name: $db"
-
-    # Extract database name without path for cleaner output filenames
-    db_name=$(basename "$db")
-
-    blastp -query "uniprot_sprot_5000.fasta" -db "$db" \
-           -out "uniprot_sprot_5000_vs_${db_name}_blastp_results.txt" \
-           -num_threads 4 \
-           -outfmt "6 qseqid sseqid stitle evalue" \
-           -max_target_seqs 1
-done
-
-# remove duplicated lines
-for f in *_blastp_db_blastp_results.txt; do 
-    echo $f; 
-    awk '!seen[$1]++ && count[$1]++ == 0' $f >$f.duprem.txt;
-done
-
-# create id file of reference
-less uniprot_sprot_5000.fasta | grep ">" | awk '{ gsub(">", ""); print $1 "\t" $0}' | awk '{ match($0, /GN=([^ ]+)/, arr); if (arr[1]) print arr[1] "\t" $0}' >uniprot_sprot_5000.fasta.id
-
-awk '{ match($0, /GN=([^ ]+)/, arr); if (arr[1]) print arr[1] "\t" $0}' uniprot_sprot_5000_vs_uniprot_sprot_10000_blastp_db_blastp_results.txt.duprem.txt | head
-
-k=uniprot_sprot_5000_vs_uniprot_sprot_10000_blastp_db_blastp_results.txt.duprem.txt
-
-for k in *_db_blastp_results.txt.duprem.txt; do
-echo $k;
-comm -12 <(less uniprot_sprot_5000.fasta.id | awk '{print $2"|"$1}' | sort) <( less $k | awk '{ match($0, /GN=([^ ]+)/, arr); if (arr[1]) print arr[1] "\t" $0}' | awk '{print $2"|"$1}' | sort) | wc -l;
-done 
+python3 annotate.py \
+    --input_faa ./data/QUERY.fasta \
+    --db ./DB/mmseq2_db.pkl \
+    --out ./data/mmseq2_result.tsv    
 
 # test blast results
 awk '$1 == $2 {count++} END {print count}' uniprot_sprot_5000_vs_uniprot_sprot_5000_blastp_db_blastp_results.txt
