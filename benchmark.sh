4d47b96c19049bd276c401bc9ac88fa2a797ac0e
Rashedul <rashedul.gen@gmail.com>
2025-04-05 23:39:00 -0700

    bmark

--- benchmark.sh
+++ benchmark.sh
@@ -1,10 +1,13 @@
 #!bin/bash
 
-# blastp 
-bash benchmark/blastp/blastp_analysis.sh
+# # blastp 
+# bash benchmark/blastp/blastp_analysis.sh
 
-# PA-SigPro 
-bash benchmark/PA-SigPro/PA-SigPro_analysis.sh 
+# # PA-SigPro 
+# bash benchmark/PA-SigPro/PA-SigPro_analysis.sh 
+
+# mds_tsne_umap
+bash benchmark/mds_tsne_umap/mds_tsne_umap_analysis.sh 
 
 # AI-Embedding (need to add model generation)
 # bash benchmark/blastp/AI-Embedding_analysis.sh 
--- benchmark/performance.sh
+++ benchmark/performance.sh
@@ -2,5 +2,7 @@
 # cd benchmark
 /usr/bin/time -v bash test.sh
 
+/usr/bin/time -v python3 annotate.py ... 2> "./benchmark/mds_tsne_umap/time_${db_name}_${m}.log"
+
 # Elapsed (wall clock) time (h:mm:ss or m:ss): 0:00.06
 # Maximum resident set size (kbytes): 6244 is the peak RAM usage 
