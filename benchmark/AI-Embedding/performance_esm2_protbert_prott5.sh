e8dce9e36428b0808eab1ee38cfb10a807b80235
Rashedul <rashedul.gen@gmail.com>
2025-08-16 15:06:29 -0700

    seq similarity update

--- benchmark/AI-Embedding/performance_esm2_protbert_prott5.sh
+++ benchmark/AI-Embedding/performance_esm2_protbert_prott5.sh
@@ -3,12 +3,12 @@
 # Clone the repo from here (https://github.com/Rajan-sust/GeneAnnotation) and run the benchmarking. 
 
 GeneAnnotation=/mnt/c/GeneAnnotation
-output=/home/rashedul/project/PA-SigPro-Pipeline/benchmark/AI-Embedding/
+output=/home/rashedul/project/ProSEC/benchmark/AI-Embedding/
 
 cd $GeneAnnotation
 
-# for model in prot_bert prot_t5 esm2; do
-for model in prot_bert prot_t5 ; do
+# for model in prot_bert prot_t5 esm2_small esm2_large; do
+for model in esm2_large; do
     for f in data/uniprot_sprot_5000.fasta; do 
         echo "Processing file: $f with model: $model"
         filename=$(basename "$f" .fasta)  
